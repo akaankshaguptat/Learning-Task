33358a1b6c71b31f5d48a0698dc498f43abdbed1
akaanksha <akaanksha.gupta@tothenew.com>
2020-07-13 13:12:38 +0530

    updated with email validation tests

--- app/src/main/java/com/example/login/util/ValidationUtils.kt
+++ app/src/main/java/com/example/login/util/ValidationUtils.kt
@@ -1,9 +1,11 @@
 package com.example.login.util
 
+import androidx.annotation.Nullable
+
 class ValidationUtils {
     companion object{
-        fun isValidEmail(email: String): Boolean {
-            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        fun isValidEmail( email: String): Boolean {
+             return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
         }
     }
 }
--- app/src/test/java/com/example/login/ExampleUnitTest.kt
+++ app/src/test/java/com/example/login/ExampleUnitTest.kt
@@ -1,17 +1,63 @@
 package com.example.login
 
-import org.junit.Test
-
+import android.content.Context
+import com.example.login.util.ValidationUtils
+import com.example.login.viewmodel.LoginViewmodel
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.runners.MockitoJUnitRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(ValidationUtils.isValidEmail("name@email.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(ValidationUtils.isValidEmail("name@email.co.uk"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(ValidationUtils.isValidEmail("name@email"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(ValidationUtils.isValidEmail("name@email..com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(ValidationUtils.isValidEmail("@email.com"))
+    }
+
+    @Test
+    fun emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(ValidationUtils.isValidEmail(""))
+    }
+
+
+
+
+
 }
